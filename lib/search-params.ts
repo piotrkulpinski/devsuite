a67293fafcfc8dfc29077f8be8d86f87651b0b41
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-01 14:40:45 +0200

    fix the search filters on tools page

--- lib/search-params.ts
+++ lib/search-params.ts
@@ -1,7 +1,7 @@
 import { createSearchParamsCache, parseAsInteger, parseAsString } from "nuqs/server"
 
 export const toolSearchParams = {
-  q: parseAsString.withDefault(""),
+  q: parseAsString,
   page: parseAsInteger.withDefault(1),
   sort: parseAsString.withDefault("publishedAt_desc"),
   perPage: parseAsInteger.withDefault(24),
