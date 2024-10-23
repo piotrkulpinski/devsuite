1a4aa1a8d17be2ef411934620131c99fd6fb3c8c
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-23 15:57:04 +0200

    remove unused session provider and fix recharts issues with react 19

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,17 +1,7 @@
-import { SessionProvider } from "next-auth/react"
 import type { PropsWithChildren } from "react"
-import { Toaster } from "~/components/admin/ui/toaster"
-import { auth } from "~/lib/auth"
 
 import "./styles.css"
 
-export default async function RootLayout({ children }: PropsWithChildren) {
-  const session = await auth()
-
-  return (
-    <SessionProvider session={session}>
-      {children}
-      <Toaster />
-    </SessionProvider>
-  )
+export default function RootLayout({ children }: PropsWithChildren) {
+  return <>{children}</>
 }
